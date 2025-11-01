fbd8c2fc23f3179ca31fdc94ea91ebd1686dd89e
Twinedline <147386342+Twinedline@users.noreply.github.com>
2025-11-01 20:47:24 +0000

    First commit
    added the setup for the tables and started inserting dummy data

---
+++ cw1_demo_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO CW1.Users (Email, Password, FirstName, LastName)
+VALUES
+('johnsmith@email.com', 'securepassword', 'John', 'Smith'),
+('erenkruger@email.com', 'securepassword', 'Eren', 'Kruger'),
+('aidenhart@email.com', 'securepassword', 'Aiden', 'Hart');
+GO
+
+asd
